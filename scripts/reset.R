57b3b8c126eed1516f1782d3128f1e8ed31cd436
Marco Chiapello <chiapello.m@gmail.com>
2023-09-26 11:26:35 +0200

    reset update

--- scripts/reset.R
+++ scripts/reset.R
@@ -1,19 +1,33 @@
-rm(list = ls())
+.rs.restartR()
+library(fs)
 library(tidyverse)
+
+# Delete everything in doc folder
+ll <- dir_ls(paste0(here::here(), "/docs"))
+dir_delete(ll[!grepl("html|json|css", ll)])
+file_delete(ll[grepl("html|json|css", ll)])
+
+# Clean presenze
 presenze <- readr::read_csv(paste0(here::here(), "/data/elencoAtlete.csv"), show_col_types = FALSE)
 presenze |> 
   mutate(date = NA,
          assenti = NA) |> 
   write_csv(paste0(here::here(), "/data/presenze.csv"))
-fs::file_delete(paste0(here::here(), "/data/presenze_old.csv"))
+presenze |> 
+  mutate(date = NA,
+         assenti = NA) |> 
+  write_csv(paste0(here::here(), "/data/presenze_old.csv"))
 
+# Clean Classifica
 presenze |> 
   mutate(vincitori = NA,
          date = NA,
          assenti = NA) |> 
   write_csv(paste0(here::here(), "/data/classificaRaw.csv"))
-fs::file_delete(paste0(here::here(), "/data/classificaRaw_old.csv"))
-
-fs::dir_delete(paste0(here::here(), "/docs"))
+presenze |> 
+  mutate(vincitori = NA,
+         date = NA,
+         assenti = NA) |> 
+  write_csv(paste0(here::here(), "/data/classificaRaw_old.csv"))
 
 
