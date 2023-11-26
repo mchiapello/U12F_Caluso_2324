032492d60f151fe5f119256516d7ef89e612f8cf
Marco Chiapello <chiapello.m@gmail.com>
2023-11-26 08:18:20 +0100

    vela - setup

--- scripts/partite/001_setup.R
+++ scripts/partite/001_setup.R
@@ -9,22 +9,22 @@ source("scripts/999_functions.R")
 
 ###############################################################################
 # Create match/allenamento
-opp <- "Frassati"
+opp <- "Vela"
 us <- "BCV Caluso"
-date <- "2023-11-17"
+date <- "2023-11-25"
 
 pp <- ma(date = date, 
          opp = opp,
          type = "partita",
-         time = "19:00:00",
+         time = "15:00:00",
          season = "2022-2023",
          league = "U12 - UISP",
          phase = "Andata",
          home_away = TRUE,
-         day_number = 2,
-         match_number = 1221106,
-         set_won = c(3, 0),
-         home_away_team  = c("a", "*"),
+         day_number = 3,
+         match_number = 122107,
+         set_won = c(2, 1),
+         home_away_team  = c("*", "a"),
          won_match = c(TRUE, FALSE),
          coach = c("Chiapello", ""),
          assistent = c("", ""))
