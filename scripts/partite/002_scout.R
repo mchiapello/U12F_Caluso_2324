c9c022026ffe601e85100d19b5e9acbd69dc06c0
Marco Chiapello <chiapello.m@gmail.com>
2024-01-22 13:10:34 +0100

    scout

--- scripts/partite/002_scout.R
+++ scripts/partite/002_scout.R
@@ -61,7 +61,7 @@ ov_scouter(dir_ls(out, regexp = "ovs$"),
 # Link Youtube video with scout
 dvw <- dir_ls(out, regexp = "dvw$")
 x <- dv_read(dvw)
-dv_meta_video(x) <- "https://youtu.be/5v8oUCb_4q4"
+dv_meta_video(x) <- "https://youtu.be/zl00WbDBg-U"
 dv_write(x, dvw)
 file_copy(dir_ls(out, regexp = "dvw$"), here("partite", "all"), overwrite = TRUE)
 
